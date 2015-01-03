8b441daa063eed8200fa308ffa09af93c02fbced
Masao Takaku <tmasao@acm.org>
2015-01-03 02:52:32 +0000

    output naid.

--- ext-candidate-distance.rb
+++ ext-candidate-distance.rb
@@ -31,7 +31,8 @@ if $0 == __FILE__
   ARGF.each do |line|
     # 著者名  論文名  雑誌名  ISSN    出版者名        出版日付        巻      号      ページ  URL     URL(DOI)
     author, title, jtitle, issn, publisher, date, vol, num, page, url, doi, = line.chomp.split( /\t/ )
-    puts line
+    #puts line
+    naid = $1 if url =~ /\/([^\/]+)\/?$/
     t_trigrams = title.trigrams
     subquery = t_trigrams.map{|e| "trigram = ?" }.join( " or " )
     trigrams = db.execute( "select * from inv_trigrams where #{ subquery } order by freq", *t_trigrams )
@@ -45,11 +46,11 @@ if $0 == __FILE__
     end
     subquery = t_trigrams.map{|e| "trigram = ?" }.join( " or " )
     bids = db.execute( "select distinct bid from trigrams where #{ subquery }", *t_trigrams )
-    puts "	searching #{ bids.size } docs..."
+    #puts "	searching #{ bids.size } docs..."
     bids.each do |bid|
       bib_id, ares_title, = db.get_first_row( "select * from article where bid = ?", bid )
       distance = distance( title, ares_title )
-      puts [ bid, [ title, ares_title ].join(" :: "), distance ].join(", ") if distance < 0.4
+      puts [ naid, bib_id, [ title, ares_title ].join(" :: ") ].join("\t") if distance < 0.3
     end
   end
 end
